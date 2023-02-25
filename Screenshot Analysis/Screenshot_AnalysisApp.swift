8c334c316f563ab445865706b6a7ffd6bb8da3f4
Avinash Jain <avinashkalpitjain@gmail.com>
2023-02-25 14:04:00 -0500

    Initial Commit

---
+++ Screenshot Analysis/Screenshot_AnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Screenshot_AnalysisApp.swift
+//  Screenshot Analysis
+//
+//  Created by Avinash Jain on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Screenshot_AnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
